3e4f75d073665bf560f29cb71b1ccc45298d15af
Naveenraj K <65030317+Naveenrajk@users.noreply.github.com>
2021-01-05 12:24:06 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+// Variable setting
+$name = $_REQUEST['name'];
+$email = $_REQUEST['Email'];
+$message = $_REQUEST['Comment'];
+
+// check input fields
+if (empty($name) || empty($email) || empty($message))
+{
+    echo "Please fill all the fields";
+}
+else 
+{
+ mail("cavecoder.dev@gmail.com", "Cavecoder Web Message", $message , "From: $name <$email>");
+    }
+   echo "<script type='text/javascript'>alert('your message sent successfully');
+         window.history.log(-1);
+         </script>";
+?>
